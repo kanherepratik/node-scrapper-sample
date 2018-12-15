40f2dee057a185e8f1b267f926147e5af16ec815
Prateek Kanhere <prateek.kanhere@coverfoxmail.com>
2018-12-15 16:30:37 +0530

    scrapping example using puppeteer

---
+++ index.js
@@ -0,0 +1,17 @@
+const puppeteer = require("puppeteer");
+const $ = require("cheerio");
+const url =
+  "https://www.nseindia.com/live_market/dynaContent/live_watch/get_quote/GetQuote.jsp?symbol=BHARTIARTL";
+
+(async () => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1920, height: 926 });
+  await page.goto(url);
+  const data = await page.evaluate(() => {
+    return document.querySelector("#companies ul.stock li.active #lastPrice")
+      .innerText;
+  });
+  console.log(data);
+  await browser.close();
+})();
